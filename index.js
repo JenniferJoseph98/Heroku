14464c416f9ef99156aa9a5b756d5982e5933155
JenniferJoseph98 <honeyjen1607@gmail.com>
2022-11-15 13:45:18 +0530

    File updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("WORKING!!!");
+  res.send("Hello world!!!");
 });
 
 app.listen(process.env.PORT || 5000);
